0f5fa9366b7d952009ccf80ca05236b220c6b722
AparicioJohan <44405276+AparicioJohan@users.noreply.github.com>
2020-03-03 07:45:18 -0500

    Update heritability_asreml.R

--- heritability_asreml.R
+++ heritability_asreml.R
@@ -1,13 +1,15 @@
-
+################
+##  ASReml v4
+################
 
 Heri.asreml <- function(Model,genotype){
   
   # Genetic variance component
-  vc.g <- summary(Model)$varcomp[paste0(genotype,"!",genotype,".var"),'component']
-  vc.g #0.142902
+  vc.g <- vc(Model)[grepl(genotype,vc(Model)$effect),2]
+  vc.g 
   
   # Mean variance of a difference of two genotypic BLUPs
-  vdBLUP.mat <- predict(Model, classify=genotype, only=genotype, sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
+  vdBLUP.mat <- predict(Model, classify=genotype, only=genotype, sed=TRUE)$sed^2 # obtain squared s.e.d. matrix 
   vdBLUP.avg <- mean(vdBLUP.mat[upper.tri(vdBLUP.mat, diag=FALSE)]) # take mean of upper triangle
   vdBLUP.avg 
   
@@ -20,14 +22,49 @@ Heri.asreml <- function(Model,genotype){
   
 }
 
-
 # For spatial design 
-Heri.AR.AR <- function(Model,Genotype){
-  
-  sp4.pv.h2 <- predict(Model, classify = Genotype, only = Genotype, sed = TRUE, control = list(trace = FALSE))
-  avepev <- mean(sp4.pv.h2$predictions$pvals$standard.error^2)
-  vcV <- sp4.pv.h2$gammas["line!line.var"]*sp4.pv.h2$sigma2
+Heri.AR.AR <- function(Model,genotype){
+  sp4.pv.h2 <- predict(Model, classify = genotype, only = genotype, sed = TRUE)
+  avepev <- mean(sp4.pv.h2$pvals$std.error^2)
+  vcV <- vc(Model)[grepl(genotype,vc(Model)$effect),2]
   h2.wn4 <- 1 - avepev/(vcV); h2.wn4[1]
-  
-  return(h2.wn4)
+  h2.wn4
 }
+
+
+################
+##  ASReml v3
+################
+
+
+#Heri.asreml <- function(Model,genotype){
+#  
+#  # Genetic variance component
+#  vc.g <- summary(Model)$varcomp[paste0(genotype,"!",genotype,".var"),'component']
+#  vc.g 
+#  
+#  # Mean variance of a difference of two genotypic BLUPs
+#  vdBLUP.mat <- predict(Model, classify=genotype, only=genotype, sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
+#  vdBLUP.avg <- mean(vdBLUP.mat[upper.tri(vdBLUP.mat, diag=FALSE)]) # take mean of upper triangle
+#  vdBLUP.avg 
+#  
+#  #############
+#  # H2 Cullis #
+#  #############
+#  H2Cullis <- 1 - (vdBLUP.avg / 2 / vc.g)
+#  
+#  return(H2Cullis)
+#  
+#}
+
+
+## For spatial design 
+#Heri.AR.AR <- function(Model,Genotype){
+#  
+#  sp4.pv.h2 <- predict(Model, classify = Genotype, only = Genotype, sed = TRUE, control = list(trace = FALSE))
+#  avepev <- mean(sp4.pv.h2$predictions$pvals$standard.error^2)
+#  vcV <- sp4.pv.h2$gammas["line!line.var"]*sp4.pv.h2$sigma2
+#  h2.wn4 <- 1 - avepev/(vcV); h2.wn4[1]
+#  
+#  return(h2.wn4)
+#}
